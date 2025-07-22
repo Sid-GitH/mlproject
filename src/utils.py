d9615b0e83ab3ee1ff9bb85285c3b8fe8a4220b5
Sid-GitH <siddharthbala.5@gmail.com>
2025-07-22 21:03:50 +0100

    model_trainer

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,10 @@ import pandas as pd
 import numpy as np 
 import dill
 
-from src.exception import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
 def save_obj(file_path, obj):
     try:
@@ -17,5 +19,31 @@ def save_obj(file_path, obj):
             dill.dump(obj,file_obj)  # to handle python objects efficiently for saving in directory
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+
+
+
+    
     
 
